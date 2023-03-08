6f3004f0328b9e3dae7380a5d1bacaeeaa25d987
Juan Manzanera <manzanera1511@gmail.com>
2023-03-08 13:16:18 -0500

    App implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,49 @@
-import React, { useEffect } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
-  const getWeather = async (latitude, longitude) => {
-    try {
-      const responde = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=1d4407cd8e297ed94b328101b6d05461`,
-      );
-      const data = await responde.json();
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [location, setLocation] = useState(null);
 
-      console.log({ data });
-    } catch (error) {
-      console.log(error);
+  
+   navigator.geolocation.getCurrentPosition(
+    (position) => {
+      setLongitude(position.coords.longitude);
+      setLatitude(position.coords.latitude);
+      // console.log(latitude)
+      console.log(longitude)
+      console.log(latitude)
     }
-  };
+  )
+    const getWeather = async () =>{
+      const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=1d4407cd8e297ed94b328101b6d05461`);
+      console.log(res.data)
+      setLocation(res.data)
+    }
 
-  useEffect(() => {
-    let latitude;
-    let longitude;
+    useEffect(() => {
+      if(latitude != null && longitude != null){
+        getWeather()
+      }
+    },[latitude])
 
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
-        getWeather(latitude, longitude);
-      },
-      (error) => {
-        throw error;
-      },
-    );
-  }, []);
+  // const getWeather = async (latitude, longitude) => {
+  //   try {
+  //     const res = await axios.get(
+  //       `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=1d4407cd8e297ed94b328101b6d05461`,
+  //     );
+
+  //       // setLocation(res.data[0])
+  //     console.log({ data });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  
   return (
-    <div classNAme="app">
+    <div className="app">
       <div className="all">
         <div className="impu">
           <input placeholder="Enter place" />
@@ -38,18 +51,18 @@ const App = () => {
         <div className="container">
           <div className="top">
             <div className="location">
-              <p>Jardin</p>
+              <p>{location?.name} </p>
             </div>
             <div className="temp">
-              <h2>19°C</h2>
+              <h2>{Math.floor(location?.main.temp - 273.15)}°C </h2>
             </div>
             <div className="description">
-              <p>sun</p>
+              <p>{location?.weather[0].description} </p>
             </div>
           </div>
           <div className="botton">
             <div className="estatus">
-              <p>viento</p>
+              <p></p>
             </div>
             <div className="presion">
               <p>presion</p>
